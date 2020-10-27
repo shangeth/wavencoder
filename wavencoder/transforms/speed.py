d6ac9e52ccc9dd3afdfc311ad0515c24ca179648
shangeth <shangethrajaa@gmail.com>
2020-10-27 20:57:42 +0530

    'audio speed change transform updated'

--- wavencoder/transforms/speed.py
+++ wavencoder/transforms/speed.py
@@ -1,19 +1,21 @@
-class SpeedChange:
+import random
+import torch
+from torchaudio.compliance import kaldi
 
-    def __init__(self, factor_range=(-0.15, 0.15), report=False):
+class SpeedChange:
+    def __init__(self, factor_range=(-0.15, 0.15), orig_freq=16000):
         self.factor_range = factor_range
-        self.report = report
+        self.orig_freq = orig_freq
 
     # @profile
-    def __call__(self, pkg):
-        pkg = format_package(pkg)
-        wav = pkg['chunk']
-        wav = wav.data.numpy().reshape(-1).astype(np.float32)
-        warp_factor = random.random() * (self.factor_range[1] - \
-                                         self.factor_range[0]) + \
-                      self.factor_range[0]
+    def __call__(self, wav):
+        wav = wav.view(-1)
+        warp_factor = random.random() * (self.factor_range[1] - self.factor_range[0]) + self.factor_range[0]
         samp_warp = wav.shape[0] + int(warp_factor * wav.shape[0])
-        rwav = signal.resample(wav, samp_warp)
+        samp_warp = 10000
+        rwav = kaldi.resample_waveform(wav.view(1, -1), self.orig_freq, samp_warp)
+        rwav = rwav.view(-1)
+
         if len(rwav) > len(wav):
             mid_i = (len(rwav) // 2) - len(wav) // 2
             rwav = rwav[mid_i:mid_i + len(wav)]
@@ -21,18 +23,14 @@ class SpeedChange:
             diff = len(wav) - len(rwav)
             P = (len(wav) - len(rwav)) // 2
             if diff % 2 == 0:
-                rwav = np.concatenate((np.zeros(P, ),
-                                       wav,
-                                       np.zeros(P, )),
+                rwav = torch.cat((torch.zeros(P, ),
+                                       rwav,
+                                       torch.zeros(P, )),
                                       axis=0)
             else:
-                rwav = np.concatenate((np.zeros(P, ),
-                                       wav,
-                                       np.zeros(P + 1, )),
+                rwav = torch.cat((torch.zeros(P, ),
+                                       rwav,
+                                       torch.zeros(P + 1, )),
                                       axis=0)
-        if self.report:
-            if 'report' not in pkg:
-                pkg['report'] = {}
-            pkg['report']['warp_factor'] = warp_factor
-        pkg['chunk'] = torch.FloatTensor(rwav)
-        return pkg
+        rwav = rwav.view(1, -1)
+        return rwav
