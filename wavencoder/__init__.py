6874c6738f37bf04c8105bf6839e495848196a44
shangeth <shangethrajaa@gmail.com>
2020-10-29 10:05:28 +0530

    'reverberation v1 added'

--- wavencoder/__init__.py
+++ wavencoder/__init__.py
@@ -16,4 +16,5 @@ from wavencoder.transforms.noise import AdditiveNoise
 from wavencoder.transforms.speed import SpeedChange
 from wavencoder.transforms.clip import Clipping
 from wavencoder.transforms.pad_crop import Pad, Crop, PadCrop
+from wavencoder.transforms.reverberation import Reverberation
 from wavencoder.transforms.compose import Compose
--- wavencoder/transforms/reverberation.py
+++ wavencoder/transforms/reverberation.py
@@ -1,5 +1,51 @@
-class Reverberation():
-    def __init__(self):
+import torch.nn.functional as F
+import torch
+import os
+import random
+import torchaudio
+
+class Reverberation:
+    def __init__(self, ir_files_dir, max_reverb_len=24000):
+        self.ir_files_dir = ir_files_dir
+        self.ir_files = os.listdir(self.ir_files_dir)
+        self.max_reverb_len = max_reverb_len
+    
+    def load_IR(self):
+        ir_file = random.choice(self.ir_files)
+        ir_file = os.path.join(self.ir_files_dir, ir_file)
+
+        IR, _ = torchaudio.load(ir_file)
+        IR = IR.view(-1)
+        IR = IR[:self.max_reverb_len]
+
+        if torch.max(IR)>0:
+            IR = IR/torch.abs(torch.max(IR))
+        p_max = torch.argmax(torch.abs(IR))
+        return IR, p_max
+
+    def shift(self, xs, n):
+        e = torch.empty_like(xs)
+        if n >= 0:
+            e[:n] = 0.0
+            e[n:] = xs[:-n]
+        else:
+            e[n:] = 0.0
+            e[:n] = xs[-n:]
+        return e
 
     def __call__(self, wav):
-        
+        wav = wav.view(-1)
+        IR, p_max = self.load_IR()
+        Ex = torch.dot(wav, wav)
+        rev = F.conv1d(wav.view(1, 1, -1), IR.view(1, 1, -1)).view(-1)
+        Er = torch.dot(rev, rev)
+        # rev = self.shift(rev, -p_max)
+        if Er>0:
+            Eratio = torch.sqrt(Ex/Er)
+        else:
+            Eratio = 1.0
+        rev = rev[:wav.shape[0]]
+        rev = Eratio * rev
+        return rev.view(1, -1)
+
+
