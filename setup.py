732775082038f34257b7ce23a0fb21b48484b2d7
shangeth <shangethrajaa@gmail.com>
2021-01-02 12:22:59 +0530

    adding attention modules

--- setup.py
+++ setup.py
@@ -10,21 +10,30 @@ setuptools.setup(
     name="wavencoder",
     author="Shangeth Rajaa",
     author_email="shangethrajaa@gmail.com",
-    version="0.0.7",
+    version="0.0.9", #0.0.7
     license="MIT",
     url="https://github.com/shangeth/wavencoder",
     description="WavEncoder - PyTorch backed audio encoder",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
     ],
     include_package_data=True,
     packages=setuptools.find_packages(),
-    install_requires=["torch", "fairseq", "tqdm"],
+    install_requires=[
+        # "torch>=1.4.0", 
+        "torchaudio",
+        "fairseq==0.9.0", 
+        "tqdm"],
+
 # line.strip() for line in open("requirements.txt", "r").readlines()],
     # dependency_links=["git+https://github.com/pytorch/fairseq"]
 )
---
+++ wavencoder/models/attention.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DotAttention(nn.Module):
+    def __inti__(self,):
+        pass
+
+    def forward(self, )
