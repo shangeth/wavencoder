7206f6182d98a2762db1a7a2b5690232b69cb318
shangeth rajaa <shangethrajaa@gmail.com>
2021-02-12 18:59:09 +0530

    added TDNN/Attention/SincNet layers; Modified package structure

---
+++ wavencoder/layers/__init__.py
@@ -0,0 +1,3 @@
+from wavencoder.layers.attention_layer import SoftAttention, DotAttention, AdditiveAttention, MultiplicativeAttention
+from wavencoder.layers.sincnet_layer import SincConvLayer
+from wavencoder.layers.tdnn import TDNN
